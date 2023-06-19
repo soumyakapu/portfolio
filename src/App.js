a434a215abf58cb35794274120a2c12ec0e67210
soumyakapu <119589183+soumyakapu@users.noreply.github.com>
2023-06-19 13:05:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
        <Navbar/>
       <Routes>
        <Route  path="/" element={<Home/>}/>
-        <Route  path="/about" element={<About/>}/>
+//         <Route  path="/about" element={<About/>}/>
         <Route  path="/portfolio" element={<Portfolio/>}/>
       </Routes>
      
